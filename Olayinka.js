f357b9115385a2a2c89b2c76820c219b18a95bdd
EgbedokunOlayinka <58890440+EgbedokunOlayinka@users.noreply.github.com>
2020-04-16 03:01:37 +0100

    Delete Olayinka.js

--- Olayinka.js
+++
@@ -1,9 +0,0 @@
-var mongo = require('mongodb');
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/Olayinka";
-
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    console.log("Database created by Olayinka");
-    db.close();
-    });
